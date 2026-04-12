c38cfe48aef3fff7e53af0dd722b401e68bbede6
queenproducejapan18-cpu <queen.produce.japan18@gmail.com>
2026-04-12 12:08:49 +0900

    sw.js の更新

--- sw.js
+++ sw.js
@@ -1,22 +1,26 @@
 'use strict';
-const CACHE = 'karute-v5';
+const CACHE = 'karute-static';
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.add('./')));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(['./', './index.html']))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
+  // ネットワーク優先 → 失敗したらキャッシュにフォールバック
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    fetch(e.request)
+      .then(res => {
+        var clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
   );
 });
